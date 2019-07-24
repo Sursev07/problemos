bdb3b5a2c5895786d564e6e9efd740011fd59758
Sursev07 <surya.simangunsong@kaskusnetworks.com>
2019-07-24 18:47:59 +0700

    Create SumMultiplies.java
    Sum Multiples of 3 and 5

---
+++ SumMultiplies.java
@@ -0,0 +1,20 @@
+public class SumMultiplies {
+    public static void main(String args[]) {
+        int result = multiplies35();
+
+        System.out.println("Sum of multiplies 3 and 5 below 1000 " + result);
+    }
+    /** 
+    ** Get sum from all numbers that multiplies 3 and 5 below 1000
+    **/
+    public static int multiplies35()
+    {
+        int result = 0;
+        for (int i = 0; i<1000; i++) {
+            if (i%3 == 0 || i%5 ==0) {
+                result = result + i;
+            }
+        }
+        return result;
+    }
+}
